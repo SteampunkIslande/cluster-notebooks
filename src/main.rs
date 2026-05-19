0a153de4848dd7e5f8d7a99a559290c0c3d58996
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-05-19 15:55:35 +0200

    Fixed pending job message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use anyhow::{Context, bail};
 use clap::Parser;
 use inquire::{Confirm, Text, validator::Validation};
 use std::io::{BufRead, Write};
@@ -71,8 +71,8 @@ fn check_squeue_status(job_id: u64) -> SqueueStatus {
         .output()
         .ok()
         .map(|res| match res.stdout.as_slice() {
-            b"PENDING" => SqueueStatus::Pending,
-            b"RUNNING" => SqueueStatus::Running,
+            b"PENDING\n" => SqueueStatus::Pending,
+            b"RUNNING\n" => SqueueStatus::Running,
             _ => SqueueStatus::AnyOther,
         })
         .unwrap_or_default()
@@ -93,16 +93,18 @@ fn main() -> anyhow::Result<()> {
     } = args;
 
     if let Some(d) = directory {
-        std::env::set_current_dir(&d).expect(&format!(
-            "Impossible de modifier le dossier courant à {}",
-            d.display()
-        ));
+        std::env::set_current_dir(&d).with_context(|| {
+            format!(
+                "Impossible de modifier le dossier courant à {}",
+                d.display()
+            )
+        })?;
     }
 
     let job_name = job_name.unwrap_or_else(|| {
         Text::new("Un nom de job est requis. Veuillez en entrer un.")
             .with_validator(|s: &str| {
-                Ok(if s.len() > 0 {
+                Ok(if !s.is_empty() {
                     Validation::Valid
                 } else {
                     Validation::Invalid("Le nom du job ne peut pas être vide".into())
@@ -145,7 +147,7 @@ fn main() -> anyhow::Result<()> {
     // Demande d'un noeud spécifique
     if let Some(ref node) = node {
         if node.contains(",") {
-            panic!(
+            bail!(
                 "Impossible de demander plusieurs noeuds pour un notebook. Il s'agit d'une restriction logique, une instance de jupyter notebook ne pouvant pas s'exécuter sur plusieurs noeuds à la fois..."
             );
         }
@@ -162,7 +164,8 @@ fn main() -> anyhow::Result<()> {
         if !Path::exists(&usermail_filename) {
             if Confirm::new("Voulez-vous ajouter votre adresse mail dans $HOME/.usermail afin d'être notifié pour les événements vous concernant ?").prompt().is_ok_and(|x|x)
             {
-                std::fs::File::create(&usermail_filename).map(|mut f|std::write!(&mut f,"{}",Text::new("Veuillez entrer votre adresse mail").prompt().expect("Vous n'avez pas entré d'adresse mail, rien n'a été fait.") )).flatten().with_context(||"Impossible de sauvegarder l'adresse mail utilisateur")?;
+                let mut f = std::fs::File::create(&usermail_filename)?;
+                std::write!(&mut f,"{}",Text::new("Veuillez entrer votre adresse mail").prompt().with_context(||"Vous n'avez pas entré d'adresse mail, rien n'a été fait.")?).with_context(||"Impossible de sauvegarder l'adresse mail utilisateur")?;
             }
         }
     }
@@ -212,14 +215,19 @@ fn main() -> anyhow::Result<()> {
                 image
             );
         }
+        let mut loop_counter = 0;
         loop {
             let st = check_squeue_status(job_id);
             match st {
                 SqueueStatus::Pending => {
-                    println!(
-                        "Votre job est toujours en attente des ressources nécessaires. Attendons 5 secondes...\n(Vous pouvez interrompre ce programme avec Ctrl+C ou kill, le job a été soumis et ne sera pas annulé. Rappel du job id SLURM: {0})\nUne fois le job alloué et démarré, vous devriez recevoir un mail, même après avoir terminé ce programme. Sinon, cherchez les logs de sortie dans /OPT/notebooks/logs/notebook-{0}.out et /OPT/notebooks/logs/notebook-{0}.err",
-                        job_id
-                    );
+                    if loop_counter > 0 {
+                        println!("Toujours en attente de ressources...");
+                    } else {
+                        println!(
+                            "Votre job est toujours en attente des ressources nécessaires. Attendons 5 secondes...\n(Vous pouvez interrompre ce programme avec Ctrl+C ou kill, le job a été soumis et ne sera pas annulé. Rappel du job id SLURM: {0})\nUne fois le job alloué et démarré, vous devriez recevoir un mail, même après avoir terminé ce programme. Sinon, cherchez les logs de sortie dans /OPT/notebooks/logs/notebook-{0}.out et /OPT/notebooks/logs/notebook-{0}.err",
+                            job_id
+                        );
+                    }
                     sleep(Duration::from_secs(5));
                 }
                 SqueueStatus::Running => {
@@ -239,6 +247,7 @@ fn main() -> anyhow::Result<()> {
                     break;
                 }
             }
+            loop_counter += 1;
         }
         Ok(())
     }
